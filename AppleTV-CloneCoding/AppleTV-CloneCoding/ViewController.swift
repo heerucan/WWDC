e99e79c890f59d2cc9ed18577f6cc53d916085ae
heerucan <iwpow0307@gmail.com>
2021-12-23 17:08:08 +0900

    [Init] AppleTV-CloneCoding Project Init(#1)

---
+++ AppleTV-CloneCoding/AppleTV-CloneCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleTV-CloneCoding
+//
+//  Created by Thisisme Hi on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
